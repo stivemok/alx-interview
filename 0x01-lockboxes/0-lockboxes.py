3c57e37efc08f1f30906bbb3faa7f678dc11022e
stivemok <stivemok@gmail.com>
2023-10-05 00:49:27 -0700

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""determines if all the boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """determines if all the boxes can be opened"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
