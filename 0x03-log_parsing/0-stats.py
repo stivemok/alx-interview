5576595be5bd611b18d005e7fbe5e1c597413acb
stivemok <stivemok@gmail.com>
2023-10-19 02:57:59 -0700

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A script: Reads standard input line by line and computes metrics"""
+
+
+def parseLogs():
+    """Reads logs from standard input and generates reports"""
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
+
+
+def report(fileSize, statusCodes):
+    """Print generated report to standard output"""
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
+
+
+if __name__ == '__main__':
+    parseLogs()
