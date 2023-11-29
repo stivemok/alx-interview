39342e8af392a3aed7db7be08fe0877644cd42f2
stivemok <stivemok@gmail.com>
2023-11-29 14:32:53 +0300

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,18 @@
+def island_perimeter(grid):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    result = 0
+
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                result += 4
+
+                if r > 0 and grid[r-1][c] == 1:
+                    result -= 2
+
+                if c > 0 and grid[r][c-1] == 1:
+                    result -= 2
+
+    return result
